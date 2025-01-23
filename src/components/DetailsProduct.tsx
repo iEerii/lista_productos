7db840dee0c85ee1ca8d5be2ef74e54f14e25b39
Ericka Ponce <erickaponce92@gmail.com>
2025-01-23 18:51:25 -0500

    create components

---
+++ src/components/DetailsProduct.tsx
@@ -0,0 +1,10 @@
+const DetailsProduct = () => {
+    return (
+        <div className="col-12">
+            <p>Detalle de producto</p>
+            <p className="bg-success text-white">Contenedor2</p>
+		</div>
+    );
+}
+ 
+export default DetailsProduct;
